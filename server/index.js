e9761dd48ad605997e842134657b80b2c3630bfd
Johannes Untoro <johannes.untoro@dt.com.au>
2018-11-06 14:20:22 +0800

    add router

--- server/index.js
+++ server/index.js
@@ -1,1 +1,24 @@
-// main starting point of the application
+// Main starting point of the application
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const router = require('./router');
+const app = express();
+
+
+
+// App Setup
+
+// morgan: logging server info
+app.use(morgan('combined'));
+// bodyParser: parsed as JSON any incoming request
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
+
+
+// Server Setup
+const port = process.env.PORT || 3090;
+const server = http .createServer(app);
+server.listen(port);
+console.log('Server is listening on port: ', port);
